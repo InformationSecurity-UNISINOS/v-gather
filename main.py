a9afbd336f536584136cf43028165a2985e7e9bb
Douglas Santos <douglas@gmail.com>
2014-08-16 17:49:37 -0300

    fix

--- main.py
+++ main.py
@@ -21,6 +21,45 @@ def usage():
     print "\t-e arquivo.txt\tExportar base de casos para texto plano"
 
 
+def StartScan():
+    GetDaemons()
+    DumpBase(nlist)
+    nlist=[]
+    nlist=OpenBase()
+    for item in nlist:
+        print "Daemon: %s"  %item.getDaemon()
+        print "Pid: %d"  %item.getDaemonPid()
+        print "Daemon Uid: %d" %item.getDaemonUid()
+        print "Daemon Gid: %d" %item.getDaemonGid()
+        print "Daemon IO Files:"
+        iof=item.getDaemonIo()
+        for token in iter(iof):
+            if token.getUname() == None:
+                user=token.getUid()
+            else:
+                user=token.getUname()
+            
+            if token.getGname() == None:
+                group=token.getGid()
+            else:
+                group=token.getGname()
+            
+            print "\t%s\t%s\t%d\t%s" %(user,group,token.getDac(),token.getFile())
+        print "Daemon Args: %s" %item.getDaemonArgs()
+
+        print "Daemon TCP port: %s" %item.getDaemonTcp()
+        print "Daemon TCP FP: %s" %item.getDaemonTcpFp()
+
+        print "Daemon UDP port: %s" %item.getDaemonUdp()
+        print "Daemon UDP FP: %s" %item.getDaemonUdpFp()
+        print "Daemon file Path: %s" %item.getFilePath()
+        print "Daemon Rpm Package: %s" %item.getDaemonRpm()
+        print "Daemon Dpkb Package: %s" %item.getDaemonDpkg()
+        print "Daemon File Dac: %d" %item.getFileDac()
+        print "Daemon File Uid: %d" %item.getFileUid()
+        print "Daemon File Gid: %d" %item.getFileGid()
+        print "*"* 150
+
 def ShowDBStatus():
     print "status beleza"
     print "existem x casos na base"
@@ -65,44 +104,6 @@ def main():
 
 
 
-
-GetDaemons()
-DumpBase(nlist)
-nlist=[]
-nlist=OpenBase()
-for item in nlist:
-        print "Daemon: %s"  %item.getDaemon()
-        print "Pid: %d"  %item.getDaemonPid()
-        print "Daemon Uid: %d" %item.getDaemonUid()
-        print "Daemon Gid: %d" %item.getDaemonGid()
-        print "Daemon IO Files:"
-        iof=item.getDaemonIo()
-        for token in iter(iof):
-            if token.getUname() == None:
-                user=token.getUid()
-            else:
-                user=token.getUname()
-            
-            if token.getGname() == None:
-                group=token.getGid()
-            else:
-                group=token.getGname()
-            
-            print "\t%s\t%s\t%d\t%s" %(user,group,token.getDac(),token.getFile())
-        print "Daemon Args: %s" %item.getDaemonArgs()
-
-        print "Daemon TCP port: %s" %item.getDaemonTcp()
-        print "Daemon TCP FP: %s" %item.getDaemonTcpFp()
-
-        print "Daemon UDP port: %s" %item.getDaemonUdp()
-        print "Daemon UDP FP: %s" %item.getDaemonUdpFp()
-        print "Daemon file Path: %s" %item.getFilePath()
-        print "Daemon Rpm Package: %s" %item.getDaemonRpm()
-        print "Daemon Dpkb Package: %s" %item.getDaemonDpkg()
-        print "Daemon File Dac: %d" %item.getFileDac()
-        print "Daemon File Uid: %d" %item.getFileUid()
-        print "Daemon File Gid: %d" %item.getFileGid()
-        print "*"* 150
 
 
 
