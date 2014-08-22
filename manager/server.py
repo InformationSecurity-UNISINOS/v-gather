c9db5e381550a2f89a46caf1caf54b2e7774bf01
Douglas Santos <douglas@gmail.com>
2014-08-22 16:15:14 -0300

    funcionou single socket, agora o teste é multithread

--- manager/server.py
+++ manager/server.py
@@ -8,12 +8,41 @@ from common import *
 from datahandler import *
 
 def StartServer():
-    server_sock = Listener((BINDIP, PORTA))
-    conn = server_sock.accept()
-    data = conn.recv()
-    print type(data)
-    print "tamanho: %d"%(len(data))
-    ProcessData(Deserialize(data))
+#    server_sock = Listener((BINDIP, PORTA))
+#    conn = server_sock.accept()
+#    data = conn.recv()
+#    print type(data)
+#    print "tamanho: %d"%(len(data))
+#    ProcessData(Deserialize(data))
+# ISSO A CIMA FUNCIONA
+
+
+    serversock = Listener((BINDIP, PORTA))
+    #serversock.bind(ADDR)
+    #serversock.listen(2)
+
+    while 1:
+        print 'waiting for connection...'
+        clientsock, addr = serversock.accept()
+        print '...connected from:', addr
+        thread.start_new_thread(thandler, (clientsock, addr))
+
+
+
+def thandler(clientsock,addr):
+    while 1:
+        data = clientsock.recv()
+        if not data:
+            break
+        print type(data)
+        print "tamanho: %d"%(len(data))
+
+    clientsock.close()
+
+
+
+
+
 
 if __name__ == "__main__":
     StartServer()
