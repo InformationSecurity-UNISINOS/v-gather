688c627c75366ff8d1cc77429ba0c7816998bd25
Douglas Santos <douglas@gmail.com>
2014-09-08 17:27:21 -0300

    testando direto base64

--- manager/parser.py
+++ manager/parser.py
@@ -11,15 +11,15 @@ def ParseBanner(string,pos):
 		for i in xrange(0,size,2):
 			tok1=string.split(':')[i]       				# port
 			tok2=string.split(':')[i+1]     				# banner/finger print
-			if not tok2.isspace() and tok2 is not "":		# if we got a banner and not a space or tabs, or any trash values
+			try:
 				banner=b64decode(tok2)  					# so decode it
-			else:
+			except:
 				banner=tok2                  				# or, just use it as is (empty)
 
 			buf=tok1+":"+banner
 			lista.append(buf)
 
-			print "DEBUG->ParseBanner: %s" %str(lista[pos])
+			print "DEBUG->ParseBanner: %s (%d)" %(str(lista[pos]),pos)
 		try:
 			return len(lista),lista[pos]
 		except:
