2784594c206a9f192a4849269cfd2a0e9fcb71c5
Douglas Santos <douglas@gmail.com>
2014-09-09 14:20:33 -0300

    gix

--- manager/match.py
+++ manager/match.py
@@ -23,7 +23,8 @@ def MatchData():
 		while qlen>0:
 			pdict = GetQueue()
 			for key in pdict.iterkeys():
-				val1, val2 = map(str, pdict[key])
+				val1, val2 = pdict[key]
+				
 				print "%s: %s" %(str(val1),str(val2))
 			qlen-=1
 	DestroyQueue()		#remover - debug
