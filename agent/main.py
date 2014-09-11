717e9eaf8f0988d8e75872a61367c7472a87ebfc
Douglas Santos <douglas@gmail.com>
2014-09-11 17:48:45 -0300

    fix loop no dry-run

--- agent/main.py
+++ agent/main.py
@@ -100,10 +100,10 @@ def StartScan(Dry):
                     continue
         
 
-        if Dry==False:
-            return 0
+        if Dry==True:
+            continue
 
-        if PingManager()==1 and Dry==False:
+        if PingManager()==1:
             SendData(server,domain,GetLinuxDist(DIST_NAME),GetLinuxDist(DIST_VER),p_pid,p_name,p_uid,p_gid,p_rpm,p_dpkg,pf_path,pf_dac,pf_uid,pf_gid,p_args,tcp_banner,udp_banner)
         else:
             print "[x] Manager offline"
