d759242e9934d117673916b19b579316a2b2f972
Douglas Santos <douglas/dot/secco/at/gmail/dot/com>
2014-09-12 15:41:19 -0300

    diminui as barras dos prints, pra ficar melhor visualmente

--- manager/match.py
+++ manager/match.py
@@ -50,11 +50,11 @@ def MatchData():
 			p_pkgm_weight = db_case['package_type_id_weight']
 			p_pkgm_score = p_pkg_weight * p_pkg_ratio
 			if debug==True:
-				print "*"*100
+				print "*"*50
 				print "Gerenciador de Pacotes: " +str(p_pkgm_ratio)
 				print "Peso: " +str(p_pkgm_weight)
 				print "Score: " +str(p_pkg_score)
-				print "*"*100
+				print "*"*50
 				print "Pacote: " +str(p_pkg_ratio)
 				print "Peso: " +str(p_pkg_weight)
 				print "Score: " +str(p_pkg_score)
@@ -66,7 +66,7 @@ def MatchData():
 			p_name_weight = db_case['process_name_weight']
 			p_name_score = p_name_ratio * p_name_weight
 			if debug==True:
-				print "*"*100
+				print "*"*50
 				print "Processo: " +str(p_name_ratio)
 				print "Peso: " +str(p_name_weight)
 				print "Score: " +str(p_name_score)
@@ -77,7 +77,7 @@ def MatchData():
 	 		p_uid_weight = db_case['process_uid_weight']
 	 		p_uid_score = p_uid_weight * p_uid_ratio
 	 		if debug==True:
-				print "*"*100
+				print "*"*50
 				print "P UID: " +str(p_uid_ratio)
 				print "Peso: " +str(p_uid_weight)
 				print "Score: " +str(p_uid_score)
@@ -88,7 +88,7 @@ def MatchData():
 			p_gid_weight = db_case['process_gid_weight']
 			p_gid_score = p_gid_ratio * p_gid_weight
 			if debug==True:
-				print "*"*100
+				print "*"*50
 				print "P GID: " +str(p_gid_ratio)
 				print "Peso: " +str(p_gid_weight)
 				print "Score: " +str(p_gid_score)
@@ -99,7 +99,7 @@ def MatchData():
 			p_args_weight = db_case['process_args_weight']
 			p_args_score = p_args_ratio * p_args_weight
 			if debug==True:
-				print "*"*100
+				print "*"*50
 				print "P ARGS: " +str(p_args_ratio)
 				print "Peso: " +str(p_args_weight)
 				print "Score: " +str(p_args_score)
@@ -110,7 +110,7 @@ def MatchData():
 			p_tcp_banner_weight = db_case['process_tcp_banner_weight']
 			p_tcp_banner_score = p_tcp_banner_ratio * p_tcp_banner_weight
 			if debug==True:
-				print "*"*100
+				print "*"*50
 				print "P TCP BANNER: " +str(p_tcp_banner_ratio)
 				print "Peso: " +str(p_tcp_banner_weight)
 				print "Score: " +str(p_tcp_banner_score)
@@ -121,7 +121,7 @@ def MatchData():
 			p_udp_banner_weight = db_case['process_udp_banner_weight']
 			p_udp_banner_score = p_udp_banner_ratio * p_udp_banner_weight
 			if debug==True:
-				print "*"*100
+				print "*"*50
 				print "P UDP BANNER: " +str(p_udp_banner_ratio)
 				print "Peso: " +str(p_udp_banner_weight)
 				print "Score: " +str(p_udp_banner_score)
@@ -132,7 +132,7 @@ def MatchData():
 			pf_path_weight = db_case['process_binary_weight']
 			pf_path_score = pf_path_ratio * pf_path_weight
 			if debug==True:
-				print "*"*100
+				print "*"*50
 				print "PF PATH: " +str(pf_path_ratio)
 				print "Peso: " +str(pf_path_weight)
 				print "Score: " +str(pf_path_score)
@@ -143,7 +143,7 @@ def MatchData():
 			pf_uid_weight = db_case['process_binary_uid_weight']
 			pf_uid_score = pf_uid_ratio * pf_uid_weight
 			if debug==True:
-				print "*"*100
+				print "*"*50
 				print "PF UID: " +str(pf_uid_ratio)
 				print "Peso: " +str(pf_uid_weight)
 				print "Score: " +str(pf_uid_score)
@@ -154,7 +154,7 @@ def MatchData():
 			pf_gid_weight = db_case['process_binary_gid_weight']
 			pf_gid_score = pf_gid_ratio * pf_gid_weight
 			if debug==True:
-				print "*"*100
+				print "*"*50
 				print "PF GID: " +str(pf_gid_ratio)
 				print "Peso: " +str(pf_gid_weight)
 				print "Score: " +str(pf_gid_score)
@@ -165,7 +165,7 @@ def MatchData():
 			pf_dac_weight = db_case['process_binary_dac_weight']
 			pf_dac_score = pf_dac_ratio * pf_dac_weight
 			if debug==True:
-				print "*"*100
+				print "*"*50
 				print "PF DAC: " +str(pf_dac_ratio)
 				print "Peso: " +str(pf_dac_weight)
 				print "Score: " +str(pf_dac_score)
@@ -176,7 +176,7 @@ def MatchData():
 			distro_version_weight = db_case['so_version_weight']
 			distro_version_score = distro_version_ratio * distro_version_weight
 			if debug==True:
-				print "*"*100
+				print "*"*50
 				print "DISTRO VERSION: " +str(distro_version_ratio)
 				print "Peso: " +str(distro_version_weight)
 				print "Score: " +str(distro_version_score)
@@ -187,7 +187,7 @@ def MatchData():
 			distro_weight = db_case['so_id_weight']
  			distro_score = distro_ratio * distro_weight
 			if debug==True:
-				print "*"*100
+				print "*"*50
 				print "DISTRO NAME: " +str(distro_ratio)
 				print "Peso: " +str(distro_weight)
 				print "Score: " +str(distro_score)
@@ -198,7 +198,7 @@ def MatchData():
 			print "DB_PNAME: "+str( db_case['process_name'])
 			print "FINAL SCORE: " +str(final_score)
 			qlen-=1				
-			print "*"*200
+			print "*"*50
 			print ""
 			print ""
 		case_id+=1
