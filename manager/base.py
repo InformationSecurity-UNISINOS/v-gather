e63d1471b12f77358a4f31ca26d4ae4e3c73a692
Douglas Santos <douglas/dot/secco/at/gmail/dot/com>
2014-09-21 15:37:23 -0300

    key fix

--- manager/base.py
+++ manager/base.py
@@ -226,7 +226,7 @@ def DbSimCases():
 							%s,%s,%s, \
 							%s,%s,%s, \
 							%s )" % ( 2, 2, str(pdict['case_id_related']), 
-								str(so_id), str(pdict['so_id_weight']), str(pdict['distro_score']),
+								str(so_id), str(pdict['distro_weight']), str(pdict['distro_score']),
 								str(pdict['distro_version']), str(pdict['distro_version_weight']), str(pdict['distro_version_score']),
 								str(pdict['p_name']),str(pdict['p_name_weight']), str(pdict['p_name_score']),
 								str(pdict['p_uid']), str(pdict['p_uid_weight']), str(pdict['p_uid_score']),
