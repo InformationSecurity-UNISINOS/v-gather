f2cd848281ed2c61ed6e564312fb2794e5e9fb49
Douglas Santos <douglas/dot/secco/at/gmail/dot/com>
2014-09-22 15:01:45 -0300

    ok agora o antes e o depois está 100%. adicionar pdict={} e pdict.clear() no inicio de tudo, resolveu o problema de inconsistencia na queue.

--- manager/match.py
+++ manager/match.py
@@ -248,13 +248,10 @@ def MatchData():
 					#DbSimCases(pdict)
 
 					candidates.AddQueue(pdict)
-					teste=candidates.GetQueue()
-					print "ANTES: %s\t%s" %(str(pdict['p_name']) , str(pdict['p_name_score']) )
-					print "DEPOIS: %s\t%s" %(str(teste['p_name']) , str(teste['p_name_score']) )
-					candidates.DestroyQueue()
+
 		case_id+=1
-	#recvdata.DestroyQueue()
-	#DbSimCases()
+	recvdata.DestroyQueue()
+	DbSimCases()
 	return True
 
 
