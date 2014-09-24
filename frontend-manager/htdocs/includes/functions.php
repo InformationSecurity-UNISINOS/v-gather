5fb1b1bd006c6f6cc3a48163a4b22bb2f7246602
Douglas Santos <douglas/dot/secco/at/gmail/dot/com>
2014-09-24 20:00:44 -0300

    debug

--- frontend-manager/htdocs/includes/functions.php
+++ frontend-manager/htdocs/includes/functions.php
@@ -8,7 +8,8 @@ require_once 'xss_clean.php';
 *
 */
 function GetWeight($mysqli, $descr) {
-
+  vardump($mysqli);
+  
   if ( $descr == "exato" ) {
     $stmt=$mysqli->prepare("select weight from weight_settings WHERE descr LIKE '%exato'%");
     $stmt->execute();
