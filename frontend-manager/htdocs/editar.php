6583e8ddb568c96af5344b38080680bfb0434d6a
Douglas Santos <douglas/dot/secco/at/gmail/dot/com>
2014-09-25 11:18:52 -0300

    update no modal

--- frontend-manager/htdocs/editar.php
+++ frontend-manager/htdocs/editar.php
@@ -1,4 +1,55 @@
-<div class="modal fade" id="edicao">
+<?php
+include_once 'includes/db_connect.php';
+include_once 'includes/functions.php';
+
+sec_session_start(); 
+if(login_check($mysqli) == false) {
+
+        header('Location: index.php');
+}
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	
+	<!-- start: Meta -->
+	<meta charset="utf-8">
+	<title>V-Gather::Manager</title>
+
+	<!-- end: Meta -->
+	
+	<!-- start: Mobile Specific -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- end: Mobile Specific -->
+	
+	<!-- start: CSS -->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="assets/css/style.min.css" rel="stylesheet">
+	<link href="assets/css/retina.min.css" rel="stylesheet">
+	<link href="assets/css/print.css" rel="stylesheet" type="text/css" media="print"/>
+	<!-- end: CSS -->
+	
+
+	<!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+		
+	  	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<script src="assets/js/respond.min.js"></script>
+		
+	<![endif]-->
+	
+	<!-- start: Favicon and Touch Icons -->
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="assets/ico/apple-touch-icon-57-precomposed.png">
+	<link rel="shortcut icon" href="assets/ico/favicon.png">
+	<!-- end: Favicon and Touch Icons -->
+</head>
+
+<body>
+	<div class="modal fade" id="edicao">
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
@@ -17,3 +68,7 @@
 			</div><!-- /.modal-content -->
 		</div><!-- /.modal-dialog -->
 	</div><!-- /.modal -->
+
+
+</body>
+</html>
