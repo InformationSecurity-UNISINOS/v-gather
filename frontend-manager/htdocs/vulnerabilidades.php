201f4628997f4bd7221b54050c60a8bd5ed01f1e
Douglas Santos <douglas/dot/secco/at/gmail/dot/com>
2014-10-04 19:02:15 -0300

    vulnerabilidades -> matching

--- frontend-manager/htdocs/vulnerabilidades.php
+++ frontend-manager/htdocs/vulnerabilidades.php
@@ -74,7 +74,7 @@ if(login_check($mysqli) == false) {
 						<li><a href="dashboard.php"><i class="fa fa-dashboard"></i><span class="hidden-sm text"> Dashboard</span></a></li>	
 						<li><a href="registrar.php"><i class="fa fa-edit"></i><span class="hidden-sm text"> Registrar Casos</span></a></li>
 						<li><a href="visualizar.php"><i class="fa fa-tags"></i><span class="hidden-sm text"> Visualizar Casos</span></a></li>
-						<li><a href="vulnerabilidades.php"><i class="fa fa-warning"></i><span class="hidden-sm text"> Vulnerabilidades</span></a></li>
+						<li><a href="matching.php"><i class="fa fa-warning"></i><span class="hidden-sm text"> Matching</span></a></li>
 						<li><a href="endpoints.php"><i class="fa fa-eye"></i><span class="hidden-sm text"> Endpoints</span></a></li>
 						<li><a href="novousuario.php"><i class="fa fa-user"></i><span class="hidden-sm text"> Usuários</span></a></li>
 						<li><a href="settings.php"><i class="fa fa-wrench"></i><span class="hidden-sm text"> Configurações</span></a></li>
@@ -93,7 +93,7 @@ if(login_check($mysqli) == false) {
 				<div class="col-sm-12 col-md-12">
 					<ol class="breadcrumb">
 					  	<li><a href="#">V-Gather</a></li>
-					  	<li class="active" >Vulnerabilidades Encontradas</li>
+					  	<li class="active" >Possíveis Casos Encontrados</li>
 					</ol>
 					
 					<div class="row">
@@ -102,7 +102,7 @@ if(login_check($mysqli) == false) {
                                 <div class="box">
                                     <div class="box-header">
                                         <h2><i class="fa fa-bookmark-o"></i>
-                                            <font face="MankSans" size="5pt">Vulnerabilidades</font>
+                                            <font face="MankSans" size="5pt">Matching</font>
                                         </h2>
                                     </div>
                                     
@@ -330,8 +330,8 @@ if(login_check($mysqli) == false) {
 	                                                   	 echo '<tr>';
 	                                                   	 	echo '<td>Deseja eleger este como melhor caso e aplicar a solução?</td>'; 
 															echo '<td colspan="4">';
-																echo '<form action="save-settings.php" method="POST" role="form" class="form-horizontal">';
-																	echo '<a data-toggle="modal" class="btn btn-danger" href="#">';
+																echo '<form action="matching.php" method="POST" role="form" class="form-horizontal">';
+																	echo '<a class="btn btn-danger" href="#">';
 																		echo ' Rejeitar  <i class="fa fa-trash-o"></i>';
 																	echo '</a>';
 																	echo '<a data-toggle="modal" class="btn btn-success" href="#">';
@@ -342,9 +342,6 @@ if(login_check($mysqli) == false) {
 															echo '</td>';
 														echo '</tr>';
 
-
-
-	                                                    	
 	                                               	echo '</tbody>';
 	                                            echo '</table>';
 											    } 
